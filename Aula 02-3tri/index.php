92fa498a35b77a23cd097da843a35dda614e6108
yuriaatom <yuri.ftom@aluno.edu.es.gov.br>
2025-09-03 11:03:43 -0300

    Update index.php

--- Aula 02-3tri/index.php
+++ Aula 02-3tri/index.php
@@ -1,1 +1,14 @@
+<htmL>
+    <head>
+        <title>Aula 02 - PHP</title>
+    </head> 
+    <body>
+        <?php
+            $nome1="Yuri";
+
+            echo "Meu nome é " .$nome1;
+        ?>
+    </body>
+</html>
+
 
