149399cbda1bdc6edc05ad023ff40b370a65a0b9
yuriaatom <yuri.ftom@aluno.edu.es.gov.br>
2025-09-30 10:50:27 -0300

    Update index.php

--- aula 05 3-tri/index.php
+++ aula 05 3-tri/index.php
@@ -12,6 +12,14 @@
             echo "<br> o resultado de $n2 <sup>$n1</sup> é igual a " .pow($n2,$n1);
             echo "<br>A raiz quadrada de $n2 é " .sqrt($n2);
 
+            echo "<br><br> O número $n2 arredondado é igual a " .round($n2);
+            echo "<br> O número $n2 arredondado pra cima é igual a " .ceil($n2);
+            echo "<br> O número $n2 arredondado para baixo é igual a " .floor($n2);
+            echo "<br><br><br>";
+
+            echo "A parte inteira de $n2 é igual a " .intval($n2);
+
+            echo "<br> Um pirulito custa R$" .number_format($n1, 2);
         ?>
     </body>         
 </html>
